0825a20bc83588134211b26fdb86d2d5244a2388
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-02 17:31:28 +0530

    Create mirror.ts

---
+++ 3d-version/src/objects/mirror.ts
@@ -0,0 +1,19 @@
+import * as THREE from 'three';
+import { Reflector } from 'three/examples/jsm/objects/Reflector';
+
+const createMirror = (width: number, height: number, position: THREE.Vector3, rotation: THREE.Euler, color: THREE.ColorRepresentation) : Reflector => {
+    const mirrorGeometry = new THREE.PlaneGeometry(width, height);
+    // Create the reflective surface
+    const reflector = new Reflector(mirrorGeometry, {
+        color: color,
+        textureWidth: window.innerWidth * window.devicePixelRatio,
+        textureHeight: window.innerHeight * window.devicePixelRatio,
+    });
+    
+    reflector.position.set(position.x, position.y, position.z);  // Position the mirror
+    reflector.rotation.set(rotation.x, rotation.y, rotation.z);  // Rotate the mirror
+
+    return reflector;
+}
+
+export { createMirror };
