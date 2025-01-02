7dbc7a2a8893ffb964b0bc25acd1e75464044adf
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-02 21:01:40 +0530

    Create constants.ts

---
+++ 3d-version/src/character/constants.ts
@@ -0,0 +1,8 @@
+const IDLE = 'Idle';
+const WALK = 'Standard Walk';
+const START_WALK = 'Start Walking';
+const STOP_WALK = 'Stop Walking';
+const TURN_LEFT = 'Left Turn';
+const TURN_RIGHT = 'Right Turn';
+
+export { IDLE, WALK, START_WALK, STOP_WALK, TURN_LEFT, TURN_RIGHT };
