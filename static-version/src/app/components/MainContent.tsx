7d9692a3eff0d63e13e4b574fe8b243e0054d8ac
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-04 17:47:47 +0530

    Update MainContent.tsx

--- static-version/src/app/components/MainContent.tsx
+++ static-version/src/app/components/MainContent.tsx
@@ -4,10 +4,17 @@ interface MainContentProps {
     renderingPage: React.ReactNode;
 }
 
-const MainContent = ({renderingPage} : MainContentProps) => {
+const MainContent = ({ renderingPage }: MainContentProps) => {
     return (
-        <div className="flex-1 p-6 bg-gray-500 h-full overflow-auto scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-300">
-            {renderingPage}
+        <div className="flex-1 bg-gray-500 flex flex-col">
+            <div className="bg-gray-800 text-gray-300 flex items-center">
+                <div className="text-sm font-medium px-4 py-1 bg-gray-700">
+                    Sample 
+                </div>
+            </div>
+            <div className="flex-1 p-5 overflow-auto scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-300">
+                {renderingPage}
+            </div>
         </div>
     );
 };
