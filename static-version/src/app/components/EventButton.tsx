5193235ab2e39c847ab14ad469861c485ac4d5fb
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-05 14:12:31 +0530

    Create components for events

---
+++ static-version/src/app/components/EventButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+interface EventButtonProps {
+    navLink: string;
+    navContent: string;
+}
+
+const EventButton = ({ navLink, navContent }: EventButtonProps) => {
+    const router = useRouter();
+    const onClick = () => {
+        router.push(navLink);
+    }
+
+    return (
+        <button 
+            className="mt-4 inline-block px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold rounded-md shadow-md transition duration-300"
+            onClick={onClick}
+        >
+            {navContent}
+        </button>
+    );
+}
+
+export default EventButton;
---
+++ static-version/src/app/components/Events.tsx
@@ -0,0 +1,50 @@
+import { faBuilding, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import EventButton from "./EventButton";
+
+export interface EventInterface {
+    title: string;
+    date: string;
+    description: string;
+    iconBgColor: string;
+    nav: string;
+    navContent: string;
+    icon?: React.ReactNode;
+}
+
+const EventGrid = ({ events }: { events: EventInterface[] }) => {
+    return (
+        <div className="relative flex flex-col items-center gap-6">
+            {events.map((event, idx) => (
+                <div key={idx} className="relative flex items-start gap-5">
+                    {idx < events.length - 1 && (
+                        <div className="absolute top-0 left-6 h-full w-px bg-gray-300 z-0"></div>
+                    )}
+
+                    <div className={`relative z-10 flex-shrink-0 w-12 h-12 flex items-center justify-center bg-${event.iconBgColor}-500 text-white rounded-full`}>
+                        {event.icon || (
+                            <span className="text-xl font-bold">
+                                <FontAwesomeIcon icon={faBuilding} />
+                            </span>
+                        )}
+                    </div>
+
+                    <div className="bg-black p-6 shadow-xl rounded-lg w-96">
+                        <h2 className="text-2xl font-semibold text-white">{event.title}</h2>
+                        <p className="text-sm text-gray-400 mt-1">{event.date}</p>
+                        <p className="mt-4 text-gray-300">{event.description}</p>
+
+                        {/* Button (Link or Custom Button) */}
+                        <EventButton
+                            navContent={event.navContent}
+                            navLink={event.nav}
+                        />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default EventGrid;
