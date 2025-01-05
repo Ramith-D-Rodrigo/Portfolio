5a828c547214d9bce207ec3e7205d0db751e3469
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-05 14:17:54 +0530

    Add content to experience and education

---
+++ static-version/src/app/education/page.tsx
@@ -0,0 +1,38 @@
+import EventGrid, { EventInterface } from "../components/Events";
+import PageTitle from "../components/PageTitle";
+
+const events: EventInterface[] = [
+    { title: "Bachelor of Science (Hons) in Computer Science", 
+        date: "2021 April - Present", 
+        description: "I started my undergraduate academics at University of Colombo School of Computing, Sri Lanka. Currently I'm a fourth/final year undergraduate with a CGPA of 3.89/4.00.",
+        iconBgColor: "blue",
+        nav: "/",
+        navContent: "Learn More"
+    },
+    { title: "High School", 
+        date: "2011 January - 2019 August", 
+        description: "From grade 6 to grade 13, I attended Thurstan College, Colombo 7 where I studied and faced both G.C.E. Ordinary Level Examination and G.C.E. Advanced Level Examination.",
+        iconBgColor: "blue",
+        nav: "/",
+        navContent: "Learn More"
+    },
+    { title: "Primary School", 
+        date: "2006 January - 2010 November", 
+        description: "From grade 1 to grade 5, I attended Sobhitha Vidyalaya, Makola where I obtained my primary education. I faced the Scholarship Examination in August 2010.",
+        iconBgColor: "blue",
+        nav: "/",
+        navContent: "Learn More"
+    }
+];
+
+const ExperiencePage = () => {
+    return (
+        <div>
+            <PageTitle title="Experience"/>
+
+            <EventGrid events={events} />
+        </div>
+    );
+}
+
+export default ExperiencePage;
---
+++ static-version/src/app/experience/page.tsx
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import EventGrid, { EventInterface } from "../components/Events";
+import PageTitle from "../components/PageTitle";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const events: EventInterface[] = [
+    {
+        date: "In the future",
+        description: "Looking for new opportunities to bring my skills to your team.",
+        title: "Excited to Join Your Team!",
+        iconBgColor: "green",
+        nav: "/",
+        navContent: "Contact",
+        icon: <FontAwesomeIcon icon={faPlus} />,
+    },
+    { title: "Software Engineering Intern", 
+        date: "2023 November - 2024 May", 
+        description: "Gained the first industrial experience by working for the Ballerina Team of WSO2 LLC. Implemented a new feature for the Ballerina programming Language.",
+        iconBgColor: "blue",
+        nav: "/",
+        navContent: "Learn More"
+    }
+];
+
+const ExperiencePage = () => {
+    return (
+        <div>
+            <PageTitle title="Experience"/>
+
+            <EventGrid events={events} />
+        </div>
+    );
+}
+
+export default ExperiencePage;
