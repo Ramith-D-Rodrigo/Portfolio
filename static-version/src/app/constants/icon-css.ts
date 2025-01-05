0788884be71e3d01fc7a6098d84b838438a21a3d
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-05 19:53:06 +0530

    Refactor the technologies and skills

---
+++ static-version/src/app/constants/icon-css.ts
@@ -0,0 +1,166 @@
+interface TechnologyWithCSSClass {
+    name: string;
+    class: string;
+};
+
+const JAVA : TechnologyWithCSSClass = {
+    name: "Java",
+    class: "devicon-java-plain-wordmark colored"
+}
+
+const CPP : TechnologyWithCSSClass = {
+    name: "C++",
+    class: "devicon-cplusplus-plain colored"
+}
+
+const C : TechnologyWithCSSClass = {
+    name: "C",
+    class: "devicon-c-plain-wordmark colored"
+}
+
+const JS : TechnologyWithCSSClass = {
+    name: "JavaScript",
+    class: "devicon-javascript-plain colored"
+}
+
+const TS : TechnologyWithCSSClass = {
+    name: "TypeScript",
+    class: "devicon-typescript-plain colored"
+}
+
+const PHP : TechnologyWithCSSClass = {
+    name: "PHP",
+    class: "devicon-php-plain colored"
+}
+
+const SCALA : TechnologyWithCSSClass = {
+    name: "Scala",
+    class: "devicon-scala-plain colored"
+}
+
+const CSHARP : TechnologyWithCSSClass = {
+    name: "C#",
+    class: "devicon-csharp-plain-wordmark colored"
+}
+
+const BALLERINA : TechnologyWithCSSClass = {
+    name: "Ballerina",
+    class: "devicon-ballerina-original-wordmark colored"
+}
+
+const PYTHON : TechnologyWithCSSClass = {
+    name: "Python",
+    class: "icon-[logos--python]"
+}
+
+const WEBGPU : TechnologyWithCSSClass = {
+    name: "WebGPU",
+    class: "icon-[devicon--webgpu-wordmark]"
+}
+
+const WEBGL : TechnologyWithCSSClass = {
+    name: "WebGL",
+    class: "icon-[simple-icons--webgl]"
+}
+
+const OPENGL : TechnologyWithCSSClass = {
+    name: "OpenGL",
+    class: "devicon-opengl-plain colored"
+}
+
+const THREEJS : TechnologyWithCSSClass = {
+    name: "Three.js",
+    class: "devicon-threejs-original-wordmark"
+}
+
+const REACTJS : TechnologyWithCSSClass = {
+    name: "React.js",
+    class: "devicon-react-original-wordmark colored"
+}
+
+const GOOGLE_DRIVE_API : TechnologyWithCSSClass = {
+    name: "Google Drive API",
+    class: "icon-[logos--google-drive]"
+}
+
+const GMAIL_API : TechnologyWithCSSClass = {
+    name: "Gmail API",
+    class: "icon-[skill-icons--gmail-light]"
+}
+
+const UNITY : TechnologyWithCSSClass = {
+    name: "Unity",
+    class: "devicon-unity-plain colored"
+}
+
+const UNREAL : TechnologyWithCSSClass = {
+    name: "Unreal Engine",
+    class: "devicon-unrealengine-original-wordmark"
+}
+
+const LARAVEL : TechnologyWithCSSClass = {
+    name: "Laravel",
+    class: "devicon-laravel-original colored"
+}
+
+const SPRINGBOOT : TechnologyWithCSSClass = {
+    name: "Spring Boot",
+    class: "devicon-spring-original colored"
+}
+
+const NEXTJS : TechnologyWithCSSClass = {
+    name: "Next.js",
+    class: "devicon-nextjs-plain"
+}
+
+const EXPRESSJS : TechnologyWithCSSClass = {
+    name: "Express.js",
+    class: "devicon-express-original"
+}
+
+const MYSQL : TechnologyWithCSSClass = {
+    name: "MySQL",
+    class: "devicon-mysql-plain-wordmark colored"
+}
+
+const MONGODB : TechnologyWithCSSClass = {
+    name: "MongoDB",
+    class: "devicon-mongodb-plain-wordmark colored"
+}
+
+const WASM : TechnologyWithCSSClass = {
+    name: "WebAssembly",
+    class: "devicon-wasm-original colored"
+}
+
+const GIT : TechnologyWithCSSClass = {
+    name: "Git",
+    class: "devicon-git-plain-wordmark colored"
+}
+
+const DOCKER : TechnologyWithCSSClass = {
+    name: "Docker",
+    class: "devicon-docker-plain-wordmark colored"
+}
+
+const UBUNTU : TechnologyWithCSSClass = {
+    name: "Ubuntu",
+    class: "devicon-ubuntu-plain colored"
+}
+
+const WINDOWS : TechnologyWithCSSClass = {
+    name: "Windows",
+    class: "devicon-windows11-original colored"
+}
+
+const HTML : TechnologyWithCSSClass = {
+    name: "HTML",
+    class: "devicon-html5-plain-wordmark colored"
+}
+
+export {
+    BALLERINA, C, CPP, CSHARP, DOCKER, EXPRESSJS, GIT, GMAIL_API, GOOGLE_DRIVE_API, HTML, JAVA,
+    JS, LARAVEL, MONGODB, MYSQL, NEXTJS, OPENGL, PHP, PYTHON, REACTJS, SCALA, SPRINGBOOT, THREEJS,
+    TS, UBUNTU, UNITY, UNREAL, WASM, WEBGL, WEBGPU, WINDOWS
+};
+export type { TechnologyWithCSSClass };
--- static-version/src/app/skills/page.tsx
+++ static-version/src/app/skills/page.tsx
@@ -1,5 +1,6 @@
 import { Noto_Sans_Sinhala } from 'next/font/google';
 import PageTitle from '../components/PageTitle';
+import { BALLERINA, C, CPP, CSHARP, DOCKER, EXPRESSJS, GIT, GMAIL_API, GOOGLE_DRIVE_API, HTML, JAVA, JS, LARAVEL, MONGODB, MYSQL, NEXTJS, OPENGL, PHP, PYTHON, REACTJS, SCALA, SPRINGBOOT, THREEJS, TS, UBUNTU, UNITY, UNREAL, WASM, WEBGL, WEBGPU } from '../constants/icon-css';
 
 const sinhalaFont = Noto_Sans_Sinhala();
 
@@ -9,46 +10,43 @@ const skills = [
         category: "Programming Languages",
         items: [
             {
-                name: "C++",
-                class: "devicon-cplusplus-plain colored",
+                tech: CPP,
                 description: "Utilizing for graphics and game programming, gaining in-depth knowledge of object-oriented programming and performance optimization techniques."
             },
             {
-                name: "Java",
-                class: "devicon-java-plain-wordmark colored",
+                tech: JAVA,
                 description: "Applied in various projects and assignments, including an internship where I utilized dynamic classloading feature."
             },
             {
-                name: "TypeScript",
-                class: "devicon-typescript-plain colored",
+                tech: TS,
                 description: "Chose for its strong typing and static analysis capabilities, which enhance code quality and maintainability in large-scale applications."
             },
             {
-                name: "PHP", class: "devicon-php-plain colored",
+                tech: PHP,
                 description: "Developed foundational web development skills through PHP, including server-side scripting and working with databases."
             },
             {
-                name: "Scala", class: "devicon-scala-plain colored",
+                tech: SCALA,
                 description: "Leveraged functional programming principles using Scala to develop scalable and maintainable applications."
             },
             {
-                name: "C#", class: "devicon-csharp-plain-wordmark colored",
+                tech: CSHARP,
                 description: "Used to build my first game, gaining experience in both game development and object-oriented programming."
             },
             {
-                name: "C", class: "devicon-c-plain-wordmark colored",
+                tech: C,
                 description: "Mastered the basics of computer programming, focusing on low-level memory management and system-level development."
             },
             {
-                name: "Ballerina", class: "devicon-ballerina-original-wordmark colored",
+                tech: BALLERINA,
                 description: "Gained hands-on experience on how to integrate services during my internship, using Ballerina for seamless connectivity."
             },
             {
-                name: "Python", class: "icon-[logos--python]",
+                tech: PYTHON,
                 description: "Applied Python for data analysis and machine learning, working with libraries like NumPy, Pandas, and TensorFlow for predictive modeling and algorithm development."
             },
             {
-                name: "JavaScript", class: "devicon-javascript-plain colored",
+                tech: JS,
                 description: "Developed dynamic, client-side web applications, focusing on DOM manipulation, event handling, and asynchronous programming."
             },
         ],
@@ -57,35 +55,36 @@ const skills = [
         category: "Libraries/APIs",
         items: [
             {
-                name: "WebGPU", class: "icon-[devicon--webgpu-wordmark]",
+                tech: WEBGPU,
                 description: "Leveraging WebGPU for cutting-edge graphics rendering in web applications, utilizing modern graphics APIs to achieve high-performance rendering."
             },
             {
-                name: "WebGL", class: "icon-[simple-icons--webgl]", color: "#990000",
+                tech: WEBGL,
+                color: "#990000",
                 description: "Built on foundational knowledge of graphics rendering on the web, working with WebGL to render 3D graphics within browsers."
             },
             {
-                name: "OpenGL", class: "devicon-opengl-plain colored",
+                tech: OPENGL,
                 description: "Acquired a strong understanding of computer graphics fundamentals and rendering techniques using OpenGL for hardware-accelerated rendering."
             },
             {
-                name: "Three.js", class: "devicon-threejs-original-wordmark",
+                tech: THREEJS,
                 description: "Using Three.js for creating 3D graphics and interactive visualizations in web-based applications, focusing on performance and ease of use."
             },
             {
-                name: "WebXR Device API",
+                tech: {name:"WebXR Device API"},
                 description: "Developing immersive web applications using WebXR to create extended reality experiences, including virtual and augmented reality applications."
             },
             {
-                name: "React.js", class: "devicon-react-original-wordmark colored",
+                tech: REACTJS,
                 description: "Built dynamic and responsive user interfaces with React.js, utilizing its component-based architecture to manage state, effects, and routing."
             },
             {
-                name: "Google Drive API", class: "icon-[logos--google-drive]",
+                tech: GOOGLE_DRIVE_API,
                 description: "Integrated Google Drive API to manage files and documents in web applications, enabling seamless storage and retrieval of user data."
             },
             {
-                name: "Gmail API", class: "icon-[skill-icons--gmail-light]",
+                tech: GMAIL_API,
                 description: "Implemented the Gmail API for sending, reading, and managing emails within web applications, allowing for rich communication features."
             },
         ],
@@ -93,46 +92,105 @@ const skills = [
     {
         category: "Engines",
         items: [
-            { name: "Unity", class: "devicon-unity-plain colored", description: "Developed interactive and immersive 3D applications using Unity, with a focus on video game development, procedural content generation, and XR applications." },
-            { name: "Unreal Engine", class: "devicon-unrealengine-original-wordmark", description: "Utilizing Unreal Engine to build high-performance video games, employing C++ for scripting and optimization of real-time rendering." },
+            { 
+                tech: UNITY,
+                description: "Developed interactive and immersive 3D applications using Unity, with a focus on video game development, procedural content generation, and XR applications."
+            },
+            { 
+                tech: UNREAL,
+                description: "Utilizing Unreal Engine to build high-performance video games, employing C++ for scripting and optimization of real-time rendering."
+            },
         ],
     },
     {
         category: "Frameworks",
         items: [
-            { name: "Laravel", class: "devicon-laravel-original colored", description: "Built robust and scalable backend APIs using Laravel, leveraging its powerful routing, authentication, and database management capabilities." },
-            { name: "Spring Boot", class: "devicon-spring-original colored", description: "Focusing on leveraging the features of Spring Boot for backend API development and integration with databases and third-party services." },
-            { name: "Next.js", class: "devicon-nextjs-plain", description: "Designed and developed server-rendered web applications using Next.js, including this portfolio website, with a focus on performance" },
-            { name: "Express.js", class: "devicon-express-original", description: "Built and deployed RESTful APIs using Express.js, ensuring efficient and scalable server-side architectures for web applications." },
+            { 
+                tech: LARAVEL, 
+                description: "Built robust and scalable backend APIs using Laravel, leveraging its powerful routing, authentication, and database management capabilities."
+            },
+            {                 
+                tech: SPRINGBOOT,
+                description: "Focusing on leveraging the features of Spring Boot for backend API development and integration with databases and third-party services." 
+            },
+            {                 
+                tech: NEXTJS,
+                description: "Designed and developed server-rendered web applications using Next.js, including this portfolio website, with a focus on performance" 
+            },
+            {                 
+                tech: EXPRESSJS,
+                description: "Built and deployed RESTful APIs using Express.js, ensuring efficient and scalable server-side architectures for web applications." 
+            },
         ],
     },
     {
         category: "Databases",
         items: [
-            { name: "MySQL", class: "devicon-mysql-plain-wordmark colored", description: "Developed a solid understanding of relational databases with MySQL, focusing on SQL queries, schema design, and performance optimization." },
-            { name: "MongoDB", class: "devicon-mongodb-plain-wordmark colored", description: "Worked with MongoDB to design and implement NoSQL databases, applying document-based storage for scalable and flexible data management." },
+            {                 
+                tech: MYSQL,
+                description: "Developed a solid understanding of relational databases with MySQL, focusing on SQL queries, schema design, and performance optimization." 
+            },
+            {                 
+                tech: MONGODB,
+                description: "Worked with MongoDB to design and implement NoSQL databases, applying document-based storage for scalable and flexible data management." 
+            },
         ],
     },
     {
         category: "Other Technical Skills",
         items: [
-            { name: "WebAssembly and Emscripten", class: "devicon-wasm-original colored", description: "Using WebAssembly and Emscripten to compile and run native code in the browser, improving performance for computation-heavy applications." },
-            { name: "Git/GitHub", class: "devicon-git-plain-wordmark colored", description: "Implementing version control practices with Git and GitHub, enabling collaborative development and efficient code management across teams." },
-            { name: "Docker", class: "devicon-docker-plain-wordmark colored", description: "Used it during the internship to implement a feature for Ballerina programming language." },
+            {                 
+                tech: WASM,
+                description: "Using WebAssembly and Emscripten to compile and run native code in the browser, improving performance for computation-heavy applications." 
+            },
+            {                 
+                tech: GIT,
+                description: "Implementing version control practices with Git and GitHub, enabling collaborative development and efficient code management across teams." 
+            },
+            {                 
+                tech: DOCKER, 
+                description: "Used it during the internship to implement a feature for Ballerina programming language." 
+            },
             {
-                name: "Ubuntu and Windows", class: ["devicon-ubuntu-plain colored", "devicon-windows11-original colored"],
+                tech: UBUNTU,
                 description: "Experienced in working with both Ubuntu and Windows operating systems, ensuring compatibility and efficiency in a cross-platform development environment."
             },
-            { name: "HTML", class: "devicon-html5-plain-wordmark colored", description: "Acquired foundational knowledge in structuring and styling web pages, using HTML to create semantic and accessible web content." },
+            {                 
+                tech: HTML,
+                description: "Acquired foundational knowledge in structuring and styling web pages, using HTML to create semantic and accessible web content." 
+            },
         ],
     },
     {
         category: "Languages Spoken",
         items: [
-            { name: "English", class: "icon-[ri--english-input]", description: "Professional working proficiency, capable of effective communication in both written and spoken contexts in technical and non-technical settings." },
-            { name: "Sinhala", class: "sinhala-letter", description: "Native proficiency in Sinhala, with fluency in both formal and colloquial speech." },
-            { name: "Japanese", class: "icon-[uil--letter-japanese-a]", description: "Have been learning Japanese for more than 4 years. Currently JLPT N4 Certified, JLPT N3 Reading." },
-            { name: "A Small Easter Egg!", description: "Did you try changing the IDE language settings? :)" }
+            { 
+                tech:
+                    {
+                        name: "English", 
+                        class: "icon-[ri--english-input]"
+                    }, 
+                description: "Professional working proficiency, capable of effective communication in both written and spoken contexts in technical and non-technical settings." },
+            { 
+                tech: {
+                name: "Sinhala", 
+                class: "sinhala-letter"
+            },
+                description: "Native proficiency in Sinhala, with fluency in both formal and colloquial speech." 
+            },
+            { 
+                tech:
+                {   name: "Japanese", 
+                    class: "icon-[uil--letter-japanese-a]"
+                }, 
+                description: "Have been learning Japanese for more than 4 years. Currently JLPT N4 Certified, JLPT N3 Reading." 
+            },
+            { 
+                tech:{
+                    name: "A Small Easter Egg!", 
+                },
+                description: "Did you try changing the IDE language settings? :)" 
+            }
         ],
     },
 ];
@@ -159,7 +217,7 @@ const SkillsPage = () => {
                                     <div className="flex items-center justify-center mb-3">
                                         {
                                             Array.isArray(item.class) ? (
-                                                item.class.map((clz, index) => (
+                                                item.tech.class.map((clz, index) => (
                                                     <i
                                                         key={index}
                                                         className={`${sinhalaFont.className} ${clz} text-9xl`}
@@ -168,19 +226,19 @@ const SkillsPage = () => {
                                                 ))
                                             ) : (
                                                 <i
-                                                    className={`${sinhalaFont.className} ${item?.class} text-9xl`}
+                                                    className={`${sinhalaFont.className} ${item.tech.class} text-9xl`}
                                                     style={{ color: item.color }}
                                                 ></i>
                                             )
                                         }
 
 
-                                        {!item.class &&
-                                            <div className="text-lg font-semibold">{item.name}</div>
+                                        {!item.tech.class &&
+                                            <div className="text-lg font-semibold">{item.tech.name}</div>
                                         }
                                     </div>
                                     {/* Description */}
-                                    <div className="text-gray-300 text-sm">{item.description}</div>
+                                    <div className="text-gray-300 text-m">{item.description}</div>
                                 </div>
 
                             ))}
