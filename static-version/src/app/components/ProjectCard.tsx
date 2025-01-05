34d8dc1ae7b732b17eaa1ca4823dbb494587edda
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-05 19:53:30 +0530

    Update ProjectCard.tsx

--- static-version/src/app/components/ProjectCard.tsx
+++ static-version/src/app/components/ProjectCard.tsx
@@ -1,13 +1,37 @@
+import { TechnologyWithCSSClass } from "../constants/icon-css";
+import EventButton from "./EventButton";
+
 interface ProjectCardProps {
     title: string;
     description: string;
+    techStack: TechnologyWithCSSClass[];
+    timePeriod: string;
 }
 
-export default function ProjectCard({ title, description }: ProjectCardProps) {
+export default function ProjectCard({ title, description, techStack, timePeriod }: ProjectCardProps) {
     return (
-        <div className="bg-white shadow rounded-lg p-4">
-        <h2 className="text-xl font-semibold">{title}</h2>
-        <p className="text-gray-600">{description}</p>
+        <div className="bg-black rounded-lg p-4 hover:scale-105 transition-transform duration-300 flex flex-col h-full">
+            {/* Main content area with title, description, and tech icons */}
+            <div className="flex-1">
+                <h2 className="text-xl font-semibold mb-3">{title}</h2>
+                {timePeriod && <p className="text-sm text-gray-300 mb-3">{timePeriod}</p>}
+
+                <p className="text-gray-300 text-m mb-3">{description}</p>
+
+                {/* Flex container for the icons */}
+                <div className="flex items-center space-x-3">
+                    {techStack.map((tech, index) => (
+                        <div key={index} className="flex items-center">
+                            <i className={`${tech.class} text-gray-300 text-5xl`} /> {/* Icon using Tailwind class */}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* EventButton at the bottom */}
+            <div className="mt-auto">
+                <EventButton navContent="Learn More" navLink="/" />
+            </div>
         </div>
     );
 }
