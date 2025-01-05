cc1f023ec9ff2d57744b0c6dbf0983014c695bc9
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-05 19:53:12 +0530

    Update page.tsx

--- static-version/src/app/experience/page.tsx
+++ static-version/src/app/experience/page.tsx
@@ -8,7 +8,7 @@ const events: EventInterface[] = [
         date: "In the future",
         description: "Looking for new opportunities to bring my skills to your team.",
         title: "Excited to Join Your Team!",
-        iconBgColor: "green",
+        iconBgColor: "red",
         nav: "/",
         navContent: "Contact",
         icon: <FontAwesomeIcon icon={faPlus} />,
