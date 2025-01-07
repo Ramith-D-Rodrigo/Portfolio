265463d2c38a0dfecfaaaa422a6dd4831213f369
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-01-07 11:18:33 +0530

    Add icon for school

--- static-version/src/app/education/page.tsx
+++ static-version/src/app/education/page.tsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import EventGrid, { EventInterface } from "../components/Events";
 import PageTitle from "../components/PageTitle";
+import { faSchool } from "@fortawesome/free-solid-svg-icons";
 
 const events: EventInterface[] = [
     { title: "Bachelor of Science (Hons) in Computer Science", 
@@ -7,21 +9,24 @@ const events: EventInterface[] = [
         description: "I started my undergraduate academics at University of Colombo School of Computing, Sri Lanka. Currently I'm a fourth/final year undergraduate with a CGPA of 3.89/4.00.",
         iconBgColor: "blue",
         nav: "/",
-        navContent: "Learn More"
+        navContent: "Learn More",
+        icon: <FontAwesomeIcon icon={faSchool} />
     },
     { title: "High School", 
         date: "2011 January - 2019 August", 
         description: "From grade 6 to grade 13, I attended Thurstan College, Colombo 7 where I studied and faced both G.C.E. Ordinary Level Examination and G.C.E. Advanced Level Examination.",
         iconBgColor: "blue",
         nav: "/",
-        navContent: "Learn More"
+        navContent: "Learn More",
+        icon: <FontAwesomeIcon icon={faSchool} />
     },
     { title: "Primary School", 
         date: "2006 January - 2010 November", 
         description: "From grade 1 to grade 5, I attended Sobhitha Vidyalaya, Makola where I obtained my primary education. I faced the Scholarship Examination in August 2010.",
         iconBgColor: "blue",
         nav: "/",
-        navContent: "Learn More"
+        navContent: "Learn More",
+        icon: <FontAwesomeIcon icon={faSchool} />
     }
 ];
 
