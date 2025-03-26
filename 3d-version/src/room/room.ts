0922e4ecb24284020723f1ffc21b97815ad240ae
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-03-26 14:12:06 +0530

    Update room.ts

--- 3d-version/src/room/room.ts
+++ 3d-version/src/room/room.ts
@@ -6,6 +6,7 @@ import { createWall, createWallPhysics } from "./wall";
 import * as THREE from "three";
 import { createObjectFromGLTF, createObjectPhysics, loadGLTFModel } from "../objects/model";
 import * as CANNON from 'cannon-es';
+import InteractableArea from "./interactable";
 
 const setupWalls = (textureLoader: THREE.TextureLoader, scene: THREE.Scene, world: CANNON.World) => {
     // 3 Walls (left, right, back)
@@ -68,6 +69,10 @@ const setupGymEquipment = async (scene: THREE.Scene, world: CANNON.World, loader
     const rack2Phy = createObjectPhysics(0, -9, 2, -1.4, 0, 0, 0, 1, 2, 2);
     world.addBody(rack2Phy);
 
+    // add the interactable collision area
+    const interactable = new InteractableArea(-9, 0, -4, 0, 0, 0, 5);
+    interactable.addToWorld(world);
+
     // load flat bench
     const flatBench = await loadGLTFModel('./models/gym_assets/flat_seat/scene.gltf', loader);
     const bench = createObjectFromGLTF(flatBench, 0, 0, -5, 0, Math.PI / 2, 0, 1, 1, 1);
@@ -94,6 +99,10 @@ const setupGymEquipment = async (scene: THREE.Scene, world: CANNON.World, loader
     const barbellPhy = createObjectPhysics(0, 7, 1, 5, 0, 0, 0, 0.8, 1, 1.6);
     world.addBody(barbellPhy);
 
+    // add the interactable collision area
+    const interactable2 = new InteractableArea(7, 1, 5, 0, 0, 0, 3);
+    interactable2.addToWorld(world);
+
     // load barbell weights
     const barbellWeights = await loadGLTFModel('./models/gym_assets/barbell_weights/scene.gltf', loader);
     const barbellWeightsObject = createObjectFromGLTF(barbellWeights, 9, 0, 8, 0, 0, 0, 1, 1, 1);
@@ -105,6 +114,10 @@ const setupGymEquipment = async (scene: THREE.Scene, world: CANNON.World, loader
     scene.add(squatRackObject);
     const squatRackPhy = createObjectPhysics(0, -9, 1.5, 5, 0, 0, 0, 1, 3, 2.5);
     world.addBody(squatRackPhy);
+
+    // add the interactable collision area
+    const interactable3 = new InteractableArea(-9, 1.5, 5, 0, 0, 0, 3.5);
+    interactable3.addToWorld(world);
 
     const allObjects = await loadGLTFModel('./models/gym_equipment/scene.gltf', loader);
     try {
@@ -131,6 +144,10 @@ const setupRoom = async (scene: THREE.Scene, world: CANNON.World, loader: GLTFLo
     setupFloor(scene, world);
     setupMirrors(scene);
     await setupGymEquipment(scene, world, loader);
+
+    // add the interactable collision area
+    const interactable = new InteractableArea(0, 0, 0, 0, 0, 0, 2);
+    interactable.addToWorld(world);
 }
 
 export { setupRoom };
