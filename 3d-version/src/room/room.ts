0c7c28354fb99ae42f8b1212d8953ba60aadbc4a
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-03-27 16:59:08 +0530

    Update room.ts

--- 3d-version/src/room/room.ts
+++ 3d-version/src/room/room.ts
@@ -6,7 +6,8 @@ import { createWall, createWallPhysics } from "./wall";
 import * as THREE from "three";
 import { createObjectFromGLTF, createObjectPhysics, loadGLTFModel } from "../objects/model";
 import * as CANNON from 'cannon-es';
-import InteractableArea from "./interactable";
+import InteractableArea from "./interactableArea";
+import Interaction from "./interaction";
 
 const setupWalls = (textureLoader: THREE.TextureLoader, scene: THREE.Scene, world: CANNON.World) => {
     // 3 Walls (left, right, back)
@@ -69,10 +70,6 @@ const setupGymEquipment = async (scene: THREE.Scene, world: CANNON.World, loader
     const rack2Phy = createObjectPhysics(0, -9, 2, -1.4, 0, 0, 0, 1, 2, 2);
     world.addBody(rack2Phy);
 
-    // add the interactable collision area
-    const interactable = new InteractableArea(-9, 0, -4, 0, 0, 0, 5);
-    interactable.addToWorld(world);
-
     // load flat bench
     const flatBench = await loadGLTFModel('./models/gym_assets/flat_seat/scene.gltf', loader);
     const bench = createObjectFromGLTF(flatBench, 0, 0, -5, 0, Math.PI / 2, 0, 1, 1, 1);
@@ -99,9 +96,6 @@ const setupGymEquipment = async (scene: THREE.Scene, world: CANNON.World, loader
     const barbellPhy = createObjectPhysics(0, 7, 1, 5, 0, 0, 0, 0.8, 1, 1.6);
     world.addBody(barbellPhy);
 
-    // add the interactable collision area
-    const interactable2 = new InteractableArea(7, 1, 5, 0, 0, 0, 3);
-    interactable2.addToWorld(world);
 
     // load barbell weights
     const barbellWeights = await loadGLTFModel('./models/gym_assets/barbell_weights/scene.gltf', loader);
@@ -114,10 +108,6 @@ const setupGymEquipment = async (scene: THREE.Scene, world: CANNON.World, loader
     scene.add(squatRackObject);
     const squatRackPhy = createObjectPhysics(0, -9, 1.5, 5, 0, 0, 0, 1, 3, 2.5);
     world.addBody(squatRackPhy);
-
-    // add the interactable collision area
-    const interactable3 = new InteractableArea(-9, 1.5, 5, 0, 0, 0, 3.5);
-    interactable3.addToWorld(world);
 
     const allObjects = await loadGLTFModel('./models/gym_equipment/scene.gltf', loader);
     try {
@@ -138,16 +128,67 @@ const setupGymEquipment = async (scene: THREE.Scene, world: CANNON.World, loader
     console.log("added all objects");
 }
 
+const setupInteractableAreas = async (scene: THREE.Scene, world: CANNON.World) => {
+    // add the interactable collision area
+    const rackInteraction = new Interaction();
+    const rackInteractionPos = new THREE.Vector3(-9, 0, -4);
+    const rackInteractionRot = new THREE.Vector3(0, 0, 0);
+    const rackTextPos = new THREE.Vector3(-7, 1, -3);
+    const rackTextRot = new THREE.Vector3(0, Math.PI/2, 0);
+    const interactable = await InteractableArea.create(
+        rackInteractionPos, rackInteractionRot, 5, 
+        "Skills", rackTextPos, rackTextRot, 
+        rackInteraction
+    );
+    interactable.addToWorld(world, scene);
+
+    // add the interactable collision area
+    const barbellInteraction = new Interaction();
+    const barbellInteractionPos = new THREE.Vector3(7, 1, 5);
+    const barbellInteractionRot = new THREE.Vector3(0, 0, 0);
+    const barbellTextPos = new THREE.Vector3(5, 1, 3);
+    const barbellTextRot = new THREE.Vector3(0, -Math.PI/2, 0);
+    const interactable2 = await InteractableArea.create(
+        barbellInteractionPos, barbellInteractionRot, 3,
+        "Experience", barbellTextPos, barbellTextRot, 
+        barbellInteraction
+    );
+    interactable2.addToWorld(world, scene);
+
+    // add the interactable collision area
+    const squatRackInteraction = new Interaction();
+    const squatRackInteractionPos = new THREE.Vector3(-9, 1.5, 5);
+    const squatRackInteractionRot = new THREE.Vector3(0, 0, 0);
+    const squatRackTextPos = new THREE.Vector3(-7, 1, 7);
+    const squatRackTextRot = new THREE.Vector3(0, Math.PI/2, 0);
+    const interactable3 = await InteractableArea.create(
+        squatRackInteractionPos, squatRackInteractionRot, 3.5, 
+        "Education", squatRackTextPos, squatRackTextRot, 
+        squatRackInteraction
+    );
+    interactable3.addToWorld(world, scene);
+
+    // add the interactable collision area
+    const warmupInteraction = new Interaction();
+    const warmupInteractionPos = new THREE.Vector3(0, 0, 0);
+    const warmupInteractionRot = new THREE.Vector3(0, 0, 0);
+    const warmupTextPos = new THREE.Vector3(-1.5, 1, 0);
+    const warmupTextRot = new THREE.Vector3(0, 0, 0);
+    const warmupInteractable = await InteractableArea.create(
+        warmupInteractionPos, warmupInteractionRot, 2,
+        "Warm Up", warmupTextPos, warmupTextRot, 
+        warmupInteraction
+    );
+    warmupInteractable.addToWorld(world, scene);
+}
+
 const setupRoom = async (scene: THREE.Scene, world: CANNON.World, loader: GLTFLoader, textureLoader: THREE.TextureLoader) => {
     setupWalls(textureLoader, scene, world);
     setupCeiling(textureLoader, scene);
     setupFloor(scene, world);
     setupMirrors(scene);
     await setupGymEquipment(scene, world, loader);
-
-    // add the interactable collision area
-    const interactable = new InteractableArea(0, 0, 0, 0, 0, 0, 2);
-    interactable.addToWorld(world);
+    await setupInteractableAreas(scene, world);
 }
 
 export { setupRoom };
