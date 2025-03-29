982c99a08f0543feb6e83d486b8a21a3d3de2ac0
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-03-29 17:48:38 +0530

    Create warmupInteractionSequence.ts

---
+++ 3d-version/src/interaction/warmupInteractionSequence.ts
@@ -0,0 +1,59 @@
+import BaseInteractionSequence from "./baseInteractionSequence";
+import * as THREE from 'three';
+import { AttachableObjectProps, InteractionAnimProps } from './interactionBuilder';
+
+class WarmupInteractionSequence extends BaseInteractionSequence {
+    public override async playSequence(characterPos: THREE.Vector3, characterRot: THREE.Quaternion,
+        camera: THREE.Camera, intCameraPos: THREE.Vector3, intCameraQuat: THREE.Quaternion, 
+        destCameraPos: THREE.Vector3, destCameraQuat: THREE.Quaternion, attachableObjects: Map<string, AttachableObjectProps>, 
+        character: THREE.Group, animationMap: Map<string, THREE.AnimationAction>, mixer: THREE.AnimationMixer, currAction: string, animations: InteractionAnimProps[]
+    ): Promise<void> {
+        const currCameraPos = camera.position.clone();
+        const currCameraOri = camera.quaternion.clone();
+        await this.interpolateCamera(camera, intCameraPos, intCameraQuat);
+        await this.interpolateCamera(camera, destCameraPos, destCameraQuat);
+
+        const currentAction = animationMap.get(currAction);
+        currentAction?.fadeOut(0.2);
+
+        for (let i = 0; i < animations.length; i++) {
+            await this.playAnimation(
+                animations[i].animName, 
+                animations[i].loop, 
+                animations[i].repeatCount, 
+                animationMap, mixer
+            );
+        }
+
+        currentAction?.reset().fadeIn(0.2).play();
+
+        await this.interpolateCamera(camera, intCameraPos, intCameraQuat);
+        await this.interpolateCamera(camera, currCameraPos, currCameraOri);
+    }
+
+    protected override playAnimation(animName: string, mode: THREE.AnimationActionLoopStyles, repetitions: number, 
+        animationMap: Map<string, THREE.AnimationAction>, mixer: THREE.AnimationMixer,): Promise<void> {
+        return new Promise((resolve) => {
+            const action = animationMap.get(animName);
+            if (!action) {
+                console.warn(`Animation '${animName}' not found`);
+                resolve();
+                return;
+            }
+            action.setLoop(mode, repetitions);
+            action.reset().fadeIn(0.2).play();
+    
+            // Listen for animation completion
+            mixer.addEventListener('finished', (event) => {
+                if (event.action === action) {
+                    action.fadeOut(0.2);
+                    resolve();
+                }
+            });
+    
+            action.clampWhenFinished = true;
+        });
+    }
+};
+
+export default WarmupInteractionSequence;
