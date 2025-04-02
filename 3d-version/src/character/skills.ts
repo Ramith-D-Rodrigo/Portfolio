507fbbebfef2956a6e56e57d198e95b90bc94a1a
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-04-02 14:06:18 +0530

    Create skills.ts

---
+++ 3d-version/src/character/skills.ts
@@ -0,0 +1,46 @@
+const SKILLS = [
+    `Programming Languages
+    
+    C
+    C++
+    Java
+    PHP
+    Python
+    JavaScript
+    TypeScript
+    C#
+    Scala`,
+
+    `Libraries
+    
+    React.js
+    WebXR
+    WebGL
+    OpenGL
+    WebGPU`,
+    
+    `Databases
+    
+    MySQL
+    MongoDB
+    PostgreSQL`,
+
+    `Frameworks
+    
+    Next.js,
+    Spring Boot,
+    Laravel,
+    Express.js`,
+
+    `Engines
+    
+    Unity
+    Unreal`,
+
+    `Other
+    
+    Docker
+    WebAssembly`
+];
+
+export default SKILLS;
