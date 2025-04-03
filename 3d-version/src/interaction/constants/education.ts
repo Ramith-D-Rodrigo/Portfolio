cc602095ef5abcbfa5228dbdc3434af756213e97
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-04-03 12:09:17 +0530

    Update education.ts

--- 3d-version/src/interaction/constants/education.ts
+++ 3d-version/src/interaction/constants/education.ts
@@ -1,48 +1,18 @@
 import { DisplayTextProps } from "../interactionBuilder";
+import {educationData} from "../../../../static-version/src/app/education/educationData";
 
-const EDUCATION: DisplayTextProps[] = [
-    {
-        title: `University of Colombo School of Computing
-        Sri Lanka
-        2021 April - 2025 May`,
-        description: `After excelling in 1st and 2nd academic years, got selected to pursue the Special (Hons) program. Thus following Bachelor of Science Honors (BSc (Hons)) in Computer Science Degree program.`
-    },
-    {
-        title: ""
-    },
-    {
-        title: ""
-    },
-    {
-        title: `University of Colombo School of Computing
-        Sri Lanka
-        2021 April - 2025 May`,
-        list: [
-            'GPA: 3.89/4.00',
-            `Director's List: 1st and 2nd semesters of the 2nd academic year.`,
-            `Director's List: 1st semester of the 3rd academic year.`
-        ]
-    },
-    {
-        title: ""
-    },
-    {
-        title: ""
-    },
-    {
-        title: `Thurstan Collage
-        Colombo 03, Sri Lanka
-        2011 January - 2019 August`,
-        description: `G.C.E. Advanced Level - 2019 August
-        Followed the Physical Science stream.
-        Results : 2As, 1B
-        
-        G.C.E Ordinary Level - 2016 December
-        Results : 8As, 1B`
-    },
-    {
-        title: ""
-    }
-];
+const EDUCATION :DisplayTextProps[] = [];
+
+educationData.forEach(edu => {
+    const newEdu: DisplayTextProps = {
+        title: `${edu.title}
+        ${edu.date}`,
+        description: edu.description,
+    };
+
+    EDUCATION.push(newEdu);
+    EDUCATION.push({title: ""});
+    EDUCATION.push({title: ""});
+});
 
 export default EDUCATION;
