742d5f75ed9fe105392df65cd9196a50e247f019
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-04-03 12:09:28 +0530

    Update skills.ts

--- 3d-version/src/interaction/constants/skills.ts
+++ 3d-version/src/interaction/constants/skills.ts
@@ -1,34 +1,16 @@
 import { DisplayTextProps } from "../interactionBuilder";
+import { skills } from "../../../../static-version/src/app/skills/skillsData/";
 
-const SKILLS: DisplayTextProps[] = [
-    {
-        title: 'Programming Languages',
-        list: ['C','C++','Java','PHP','Python','JavaScript', 'TypeScript','C#','Scala', 'Ballerina']
-    },
-    {
-        title: 'Libraries',
-        list: ['React.js', 'WebXR Device API', 'WebGL', 'WebGPU', 'OpenGL']
-    },
-    {
-        title: 'Databases',
-        list: ['MySQL', 'MongoDB', 'PostgreSQL']
-    },
-    {
-        title: 'Engines',
-        list: ['Unity','Unreal']
-    },
-    {
-        title: 'Frameworks',
-        list: ['Next.js', 'Spring Boot', 'Laravel', 'Express.js']
-    },
-    {
-        title: 'Other',
-        list: ['Docker', 'WebAssembly', 'Git', 'Ubuntu']
-    },
-    {
-        title: 'Languages Spoken',
-        list: ['Sinhala (Native)', 'English (Professional)', 'Japanese (JLPT N3)']
-    },
-];
+const SKILLS: DisplayTextProps[] = [];
+
+skills.forEach(skill => {
+    const newSkill : DisplayTextProps = {
+        title: `${skill.category}`,
+        list: skill.items.map(item => item.tech.name)
+    };
+
+    SKILLS.push(newSkill);
+});
+
 
 export default SKILLS;
