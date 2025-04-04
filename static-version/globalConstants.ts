fceb8b9da300bdcb97593f6774af94958c91c1c9
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-04-04 14:27:13 +0530

    Create globalConstants.ts

---
+++ static-version/globalConstants.ts
@@ -0,0 +1,7 @@
+const LINKEDIN = "linkedin";
+const GITHUB = "github";
+
+const LINKEDIN_URL = "https://www.linkedin.com/in/ramith-d-rodrigo/";
+const GITHUB_URL = "https://github.com/Ramith-D-Rodrigo";
+
+export {LINKEDIN, GITHUB, LINKEDIN_URL, GITHUB_URL};
