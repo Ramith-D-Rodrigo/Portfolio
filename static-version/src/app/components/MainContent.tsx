4129f03a9e5f5194912bee2689e6acf2484e9a11
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-04-04 13:55:46 +0530

    Update MainContent.tsx

--- static-version/src/app/components/MainContent.tsx
+++ static-version/src/app/components/MainContent.tsx
@@ -7,7 +7,7 @@ interface MainContentProps {
 
 const MainContent = ({ renderingPage }: MainContentProps) => {
     return (
-        <div className="flex-1 flex flex-col text-white overflow-visible">
+        <div className="bg-gray-800 flex-1 flex flex-col text-white overflow-visible">
             <Tabs/>
             <div id="pageDisplayer" className="flex-1 p-5 overflow-auto scrollbar scrollbar-thumb-gray-600 scrollbar-track-gray-800">
                 {renderingPage}
