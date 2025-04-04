08983f5f06ad3f52173f982f93de443aca031d48
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-04-04 13:55:43 +0530

    Update Footer.tsx

--- static-version/src/app/components/Footer.tsx
+++ static-version/src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="text-center py-2 text-gray-100">
+        <footer className="text-center py-2 text-gray-100 bg-gray-800">
         <p>© 2025 Ramith Rodrigo. All rights reserved.</p>
         </footer>
     );
