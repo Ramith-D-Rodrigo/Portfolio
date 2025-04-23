008c1a0e80ade07a214cd63b251f8b4211c94e7d
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2025-04-23 13:47:57 +0530

    Create SubHeader.tsx

---
+++ static-version/src/app/components/SubHeader.tsx
@@ -0,0 +1,9 @@
+const SubHeader = ({content}) => {
+    return (
+        <div className="text-2xl font-semibold mb-4">
+            {content}
+        </div>
+    );
+}
+
+export default SubHeader;
