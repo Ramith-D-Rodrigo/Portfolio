15f2091cbd7a8e0cca73853f3d96c65c707af75f
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2026-06-07 13:07:07 +0530

    Update skills

--- static-version/src/app/constants/icon-css.ts
+++ static-version/src/app/constants/icon-css.ts
@@ -1,176 +1,212 @@
 interface TechnologyWithCSSClass {
-    name: string;
-    class?: string;
+  name: string;
+  class?: string;
+}
+
+const JAVA: TechnologyWithCSSClass = {
+  name: "Java",
+  class: "devicon-java-plain-wordmark colored",
 };
 
-const JAVA : TechnologyWithCSSClass = {
-    name: "Java",
-    class: "devicon-java-plain-wordmark colored"
-}
+const CPP: TechnologyWithCSSClass = {
+  name: "C++",
+  class: "devicon-cplusplus-plain colored",
+};
 
-const CPP : TechnologyWithCSSClass = {
-    name: "C++",
-    class: "devicon-cplusplus-plain colored"
-}
+const C: TechnologyWithCSSClass = {
+  name: "C",
+  class: "devicon-c-plain-wordmark colored",
+};
 
-const C : TechnologyWithCSSClass = {
-    name: "C",
-    class: "devicon-c-plain-wordmark colored"
-}
+const JS: TechnologyWithCSSClass = {
+  name: "JavaScript",
+  class: "devicon-javascript-plain colored",
+};
 
-const JS : TechnologyWithCSSClass = {
-    name: "JavaScript",
-    class: "devicon-javascript-plain colored"
-}
+const TS: TechnologyWithCSSClass = {
+  name: "TypeScript",
+  class: "devicon-typescript-plain colored",
+};
 
-const TS : TechnologyWithCSSClass = {
-    name: "TypeScript",
-    class: "devicon-typescript-plain colored"
-}
+const PHP: TechnologyWithCSSClass = {
+  name: "PHP",
+  class: "devicon-php-plain colored",
+};
 
-const PHP : TechnologyWithCSSClass = {
-    name: "PHP",
-    class: "devicon-php-plain colored"
-}
+const SCALA: TechnologyWithCSSClass = {
+  name: "Scala",
+  class: "devicon-scala-plain colored",
+};
 
-const SCALA : TechnologyWithCSSClass = {
-    name: "Scala",
-    class: "devicon-scala-plain colored"
-}
+const CSHARP: TechnologyWithCSSClass = {
+  name: "C#",
+  class: "devicon-csharp-plain-wordmark colored",
+};
 
-const CSHARP : TechnologyWithCSSClass = {
-    name: "C#",
-    class: "devicon-csharp-plain-wordmark colored"
-}
+const BALLERINA: TechnologyWithCSSClass = {
+  name: "Ballerina",
+  class: "devicon-ballerina-original-wordmark colored",
+};
 
-const BALLERINA : TechnologyWithCSSClass = {
-    name: "Ballerina",
-    class: "devicon-ballerina-original-wordmark colored"
-}
+const PYTHON: TechnologyWithCSSClass = {
+  name: "Python",
+  class: "icon-[logos--python]",
+};
 
-const PYTHON : TechnologyWithCSSClass = {
-    name: "Python",
-    class: "icon-[logos--python]"
-}
+const WEBGPU: TechnologyWithCSSClass = {
+  name: "WebGPU",
+  class: "icon-[devicon--webgpu-wordmark]",
+};
 
-const WEBGPU : TechnologyWithCSSClass = {
-    name: "WebGPU",
-    class: "icon-[devicon--webgpu-wordmark]"
-}
+const WEBGL: TechnologyWithCSSClass = {
+  name: "WebGL",
+  class: "icon-[simple-icons--webgl]",
+};
 
-const WEBGL : TechnologyWithCSSClass = {
-    name: "WebGL",
-    class: "icon-[simple-icons--webgl]"
-}
+const OPENGL: TechnologyWithCSSClass = {
+  name: "OpenGL",
+  class: "devicon-opengl-plain colored",
+};
 
-const OPENGL : TechnologyWithCSSClass = {
-    name: "OpenGL",
-    class: "devicon-opengl-plain colored"
-}
+const THREEJS: TechnologyWithCSSClass = {
+  name: "Three.js",
+  class: "devicon-threejs-original-wordmark",
+};
 
-const THREEJS : TechnologyWithCSSClass = {
-    name: "Three.js",
-    class: "devicon-threejs-original-wordmark"
-}
+const REACTJS: TechnologyWithCSSClass = {
+  name: "React.js",
+  class: "devicon-react-original-wordmark colored",
+};
 
-const REACTJS : TechnologyWithCSSClass = {
-    name: "React.js",
-    class: "devicon-react-original-wordmark colored"
-}
+const GOOGLE_DRIVE_API: TechnologyWithCSSClass = {
+  name: "Google Drive API",
+  class: "icon-[logos--google-drive]",
+};
 
-const GOOGLE_DRIVE_API : TechnologyWithCSSClass = {
-    name: "Google Drive API",
-    class: "icon-[logos--google-drive]"
-}
+const GMAIL_API: TechnologyWithCSSClass = {
+  name: "Gmail API",
+  class: "icon-[skill-icons--gmail-light]",
+};
 
-const GMAIL_API : TechnologyWithCSSClass = {
-    name: "Gmail API",
-    class: "icon-[skill-icons--gmail-light]"
-}
+const UNITY: TechnologyWithCSSClass = {
+  name: "Unity",
+  class: "devicon-unity-plain colored",
+};
 
-const UNITY : TechnologyWithCSSClass = {
-    name: "Unity",
-    class: "devicon-unity-plain colored"
-}
+const UNREAL: TechnologyWithCSSClass = {
+  name: "Unreal Engine",
+  class: "devicon-unrealengine-original-wordmark",
+};
 
-const UNREAL : TechnologyWithCSSClass = {
-    name: "Unreal Engine",
-    class: "devicon-unrealengine-original-wordmark"
-}
+const LARAVEL: TechnologyWithCSSClass = {
+  name: "Laravel",
+  class: "devicon-laravel-original colored",
+};
 
-const LARAVEL : TechnologyWithCSSClass = {
-    name: "Laravel",
-    class: "devicon-laravel-original colored"
-}
+const SPRINGBOOT: TechnologyWithCSSClass = {
+  name: "Spring Boot",
+  class: "devicon-spring-original colored",
+};
 
-const SPRINGBOOT : TechnologyWithCSSClass = {
-    name: "Spring Boot",
-    class: "devicon-spring-original colored"
-}
+const NEXTJS: TechnologyWithCSSClass = {
+  name: "Next.js",
+  class: "devicon-nextjs-plain",
+};
 
-const NEXTJS : TechnologyWithCSSClass = {
-    name: "Next.js",
-    class: "devicon-nextjs-plain"
-}
+const NESTJS: TechnologyWithCSSClass = {
+  name: "Nest.js",
+  class: "devicon-nestjs-original colored",
+};
 
-const EXPRESSJS : TechnologyWithCSSClass = {
-    name: "Express.js",
-    class: "devicon-express-original"
-}
+const EXPRESSJS: TechnologyWithCSSClass = {
+  name: "Express.js",
+  class: "devicon-express-original",
+};
 
-const MYSQL : TechnologyWithCSSClass = {
-    name: "MySQL",
-    class: "devicon-mysql-plain-wordmark colored"
-}
+const MYSQL: TechnologyWithCSSClass = {
+  name: "MySQL",
+  class: "devicon-mysql-plain-wordmark colored",
+};
 
-const MONGODB : TechnologyWithCSSClass = {
-    name: "MongoDB",
-    class: "devicon-mongodb-plain-wordmark colored"
-}
+const MONGODB: TechnologyWithCSSClass = {
+  name: "MongoDB",
+  class: "devicon-mongodb-plain-wordmark colored",
+};
 
-const POSTGRESQL : TechnologyWithCSSClass = {
-    name: "PostgreSQL",
-    class: "devicon-postgresql-plain-wordmark colored"
-}
+const POSTGRESQL: TechnologyWithCSSClass = {
+  name: "PostgreSQL",
+  class: "devicon-postgresql-plain-wordmark colored",
+};
 
-const WASM : TechnologyWithCSSClass = {
-    name: "WebAssembly",
-    class: "devicon-wasm-original colored"
-}
+const WASM: TechnologyWithCSSClass = {
+  name: "WebAssembly",
+  class: "devicon-wasm-original colored",
+};
 
-const GIT : TechnologyWithCSSClass = {
-    name: "Git",
-    class: "devicon-git-plain-wordmark colored"
-}
+const GIT: TechnologyWithCSSClass = {
+  name: "Git",
+  class: "devicon-git-plain-wordmark colored",
+};
 
-const DOCKER : TechnologyWithCSSClass = {
-    name: "Docker",
-    class: "devicon-docker-plain-wordmark colored"
-}
+const DOCKER: TechnologyWithCSSClass = {
+  name: "Docker",
+  class: "devicon-docker-plain-wordmark colored",
+};
 
-const UBUNTU : TechnologyWithCSSClass = {
-    name: "Ubuntu",
-    class: "devicon-ubuntu-plain colored"
-}
+const UBUNTU: TechnologyWithCSSClass = {
+  name: "Ubuntu",
+  class: "devicon-ubuntu-plain colored",
+};
 
-const WINDOWS : TechnologyWithCSSClass = {
-    name: "Windows",
-    class: "devicon-windows11-original colored"
-}
+const WINDOWS: TechnologyWithCSSClass = {
+  name: "Windows",
+  class: "devicon-windows11-original colored",
+};
 
-const HTML : TechnologyWithCSSClass = {
-    name: "HTML",
-    class: "devicon-html5-plain-wordmark colored"
-}
+const HTML: TechnologyWithCSSClass = {
+  name: "HTML",
+  class: "devicon-html5-plain-wordmark colored",
+};
 
-const WEBXR : TechnologyWithCSSClass = {
-    name: "WebXR Device API",
-    class: "webxr-logo"
-}
+const WEBXR: TechnologyWithCSSClass = {
+  name: "WebXR Device API",
+  class: "webxr-logo",
+};
 
 export {
-    BALLERINA, C, CPP, CSHARP, DOCKER, EXPRESSJS, GIT, GMAIL_API, GOOGLE_DRIVE_API, HTML, JAVA,
-    JS, LARAVEL, MONGODB, MYSQL, POSTGRESQL, NEXTJS, OPENGL, PHP, PYTHON, REACTJS, SCALA, SPRINGBOOT, THREEJS,
-    TS, UBUNTU, UNITY, UNREAL, WASM, WEBGL, WEBGPU, WINDOWS, WEBXR
+  BALLERINA,
+  C,
+  CPP,
+  CSHARP,
+  DOCKER,
+  EXPRESSJS,
+  GIT,
+  GMAIL_API,
+  GOOGLE_DRIVE_API,
+  HTML,
+  JAVA,
+  JS,
+  LARAVEL,
+  MONGODB,
+  MYSQL,
+  POSTGRESQL,
+  NEXTJS,
+  OPENGL,
+  PHP,
+  PYTHON,
+  REACTJS,
+  SCALA,
+  SPRINGBOOT,
+  THREEJS,
+  TS,
+  UBUNTU,
+  UNITY,
+  UNREAL,
+  WASM,
+  WEBGL,
+  WEBGPU,
+  WINDOWS,
+  WEBXR,
+  NESTJS,
 };
 export type { TechnologyWithCSSClass };
